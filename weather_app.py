696d2a869ff3ecc2c07c963855de4c065259fe7a
Tlam3358 <50388430+Tlam3358@users.noreply.github.com>
2024-04-20 13:30:57 -0700

    Add files via upload

---
+++ weather_app.py
@@ -0,0 +1,15 @@
+import requests
+
+api_key = 'ae52e357746120cfdba88061c1af96d6'
+
+def get_weather(city):
+    weather_data = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&APPID={api_key}")
+
+    if weather_data.json()['cod'] == '404':
+        return None
+    else:
+        weather = weather_data.json()['weather'][0]['main']
+        temp = round(weather_data.json()['main']['temp'])
+        return {'weather': weather, 'temp': temp}
+
